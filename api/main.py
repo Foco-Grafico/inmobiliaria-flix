1046d56d6e43bd0cf8177f8521a380d4170d11c4
KikeDeAlba <likingvirus1@gmail.com>
2024-05-01 00:08:56 -0600

    Refactor main.py to include database query for users table in root endpoint

--- api/main.py
+++ api/main.py
@@ -1,4 +1,5 @@
 from api.src.core.app import FAST_APP
+from api.src.services.turso import DB
 
 fast_app = FAST_APP(
     routers=[]
@@ -8,5 +9,9 @@ app = fast_app.get_app()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello world"}
+    db = DB()
+
+    _, schema = db.select().from_table('users').execute()
+
+    return {"message": schema}
 
