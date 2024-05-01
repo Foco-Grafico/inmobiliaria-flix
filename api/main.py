6dddee1a640a7160f3554481374d3e34ee088bff
KikeDeAlba <likingvirus1@gmail.com>
2024-04-30 19:42:30 -0700

    Refactor API initialization in main.py and app.py

--- api/main.py
+++ api/main.py
@@ -1,7 +1,3 @@
 from src.core.app import APP
 
-app = APP().get_app()
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+app = APP()
--- api/src/core/app.py
+++ api/src/core/app.py
@@ -3,6 +3,3 @@ from fastapi import FastAPI
 class APP:
     def __init__(self):
         self.app = FastAPI()
-
-    def get_app(self):
-        return self.app
