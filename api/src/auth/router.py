8f69e89efcdd69ab60c197919f31fd0e48f08466
KikeDeAlba <likingvirus1@gmail.com>
2024-05-01 02:44:33 -0600

    Refactor API router to include login functionality in auth/router.py

--- api/src/auth/router.py
+++ api/src/auth/router.py
@@ -1,9 +1,10 @@
 from fastapi import APIRouter
-from api.src.auth.controllers.register import register_user
+from api.src.auth.controllers import login, register
 
 router = APIRouter(
     prefix="/auth",
     tags=["auth"],
 )
 
-router.post("/register")(register_user)
+router.post("/register")(register.register_user)
+router.post("/login")(login.login_user)
