39826a3a60bad839015fecfe0449cfc49992cf54
KikeDeAlba <likingvirus1@gmail.com>
2024-04-30 20:12:06 -0700

    Refactor API initialization and app structure

--- api/main.py
+++ api/main.py
@@ -1,15 +1,10 @@
-from src.core.app import FAST_APP
+from .src.core.app import FAST_APP
 from dotenv import load_dotenv
 load_dotenv()
-from os import environ
 
 app = FAST_APP().get_app()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Hello world"}
 
-if __name__ == "__main__":
-    import uvicorn
-
-    uvicorn.run(app, host="0.0.0.0", port=8000)
