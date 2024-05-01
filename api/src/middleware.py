f98eebfbefa289ac612f7d817804a7cc79fa81c8
KikeDeAlba <likingvirus1@gmail.com>
2024-04-30 21:20:07 -0700

    Refactor API initialization to include new middleware logic and print request cookies

--- api/src/middleware.py
+++ api/src/middleware.py
@@ -1,8 +1,11 @@
 from fastapi import Request, Response
+# from fastapi.responses import RedirectResponse
 from typing import Callable
 
 async def middleware(
     request: Request,
     next: Callable[[], Response]
 ):
+    print(request.cookies.items())
+
     return next()
