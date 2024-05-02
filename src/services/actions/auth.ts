c214aeab5dfa8973df3d8e8624b690994d3975ad
KikeDeAlba <likingvirus1@gmail.com>
2024-05-02 16:12:32 -0600

    Refactor Login function to use Zod package for error handling and redirect based on response

--- src/services/actions/auth.ts
+++ src/services/actions/auth.ts
@@ -1,17 +1,17 @@
 'use server'
 
-import { redirect } from "next/navigation";
+import { redirect } from 'next/navigation'
+import { z } from 'zod'
 
 export const Login = async (data: FormData) => {
-    const response = await fetch('/api/login', {
-        method: 'POST',
-        body: data
-    });
+  const response = await fetch('/api/login', {
+    method: 'POST',
+    body: data
+  })
 
-    const json = await response.json()
-    
-    
-    const newPath = response.ok ? '/' : `/login?error=${json.detail ?? ''}`
+  const json = await response.json()
 
-    redirect(newPath)
+  const newPath = response.ok ? '/' : `/login?error=${z.string().parse(json.detail) ?? ''}`
+
+  redirect(newPath)
 }
