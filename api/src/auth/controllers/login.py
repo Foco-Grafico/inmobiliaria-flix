215fae124d15be79430bfb90006bcd635453d593
KikeDeAlba <likingvirus1@gmail.com>
2024-05-02 16:51:15 -0600

    Refactor login_user function to handle error redirects and current_path parameter

--- api/src/auth/controllers/login.py
+++ api/src/auth/controllers/login.py
@@ -24,8 +24,12 @@ class LoginUserRequest(BaseModel):
     )
 
 
-async def login_user(redirect: str | None = None, request_user: LoginUserRequest = Depends(LoginUserRequest.as_form)):
+def decode_url(url: str):
+    return url.replace('%2F', '/')
+
+async def login_user(redirect: str | None = None, current_path: str | None = None, request_user: LoginUserRequest = Depends(LoginUserRequest.as_form)):
     db = DB()
+
     try:
         user = db.select(['token']).from_table('users').where({
             'email': request_user.email
@@ -33,18 +37,27 @@ async def login_user(redirect: str | None = None, request_user: LoginUserRequest
             'phone_number': request_user.phone_number
         }).single_execute()
     except:
+        if current_path is not None:
+            return RedirectResponse(url=f'{decode_url(current_path)}?error=An error has occurred')
+
         # Error has occurred when user is not found
-        raise HTTPException(status_code=404, detail='User not found')
+        raise HTTPException(status_code=404, detail='An error has occurred')
     
     if user is None:
         # User is not found
+        if current_path is not None:
+            return RedirectResponse(url=f'{decode_url(current_path)}?error=User not found')
+
         raise HTTPException(status_code=404, detail='User not found')
     
     if not checkpw(request_user.password.encode('utf-8'), user['token'].encode('utf-8')):
+        if current_path is not None:
+            return RedirectResponse(url=f'{decode_url(current_path)}?error=Invalid password')
+
         raise HTTPException(status_code=401, detail='Invalid password')
 
     if redirect is not None:
-        response = RedirectResponse(url=redirect)
+        response = RedirectResponse(url=decode_url(redirect))
     else:
         response = JSONResponse(content={
             'message': 'User logged in successfully',
