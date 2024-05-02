201b257ce8fe2f4644bc1309096bf1d726618ff1
KikeDeAlba <likingvirus1@gmail.com>
2024-05-02 15:04:47 -0600

    Add get_user and is_logged API endpoints for user authentication

--- api/src/auth/router.py
+++ api/src/auth/router.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from api.src.auth.controllers import login, register
+from api.src.auth.controllers import login, register, get_user, is_logged
 
 router = APIRouter(
     prefix="/auth",
@@ -8,3 +8,5 @@ router = APIRouter(
 
 router.post("/register")(register.register_user)
 router.post("/login")(login.login_user)
+router.get("/user")(get_user.get_user)
+router.get("/is-logged")(is_logged.is_logged)
