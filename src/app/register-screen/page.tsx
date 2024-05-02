77f09fe6d9072184e1c9aed8a4eea1da69858df4
JesusOlivas12 <chapo7503@gmail.com>
2024-05-01 19:51:32 -0600

    Creating front end logging function

---
+++ src/app/register-screen/page.tsx
@@ -0,0 +1,46 @@
+import { error } from "console";
+import { METHODS } from "http";
+
+export default function FormRegister({onSubmit}) {
+    const handleSubmit = async (e:React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        const formData = new FormData (e.currentTarget);
+
+        fetch ('api/register'{
+            method: 'POST',
+            body: formData,
+            })
+ 
+            .then (response => {
+                if (!response.ok) {
+                    throw new Error('Error al registrarse');
+                }
+                return response.json();
+            })
+
+            .then (data => {
+                console.log ('Regristro exitoso');
+            })
+
+            .catch (error => {
+                console.error('Error al registrase', error.message || 'Error desconocido');
+            })
+            
+    return (
+        <form onSubmit={handleSubmit}>
+            <input type="text" name="first_name" placeholder="Juan"></input>
+            <input type="text" name="last_name" placeholder="Hernandez"></input>
+            <input type="email" name="email" placeholder="example@gmail,com" ></input>
+            <input type="password" name="passeword" placeholder="*******"></input>
+            <input type="text" name="phone_number" placeholder="669122133"></input>
+            <button type="submit">Registrarse</button>
+        </form>
+    )
+
+
+
+        }
+
+        }
+
