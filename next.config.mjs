4ea9c89656acb0610457de6921f046d7162eca9e
KikeDeAlba <likingvirus1@gmail.com>
2024-05-02 14:19:24 -0600

    Add API endpoints for /api/ and /docs/ in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,13 @@ const nextConfig = {
                 process.env.NODE_ENV === "development"
                   ? "http://127.0.0.1:8000/:path*"
                   : "/api/",
+            },
+            {
+              source: "/docs/:path*",
+              destination:
+                process.env.NODE_ENV === "development"
+                  ? "http://127.0.0.1:8000/docs/:path*"
+                  : "/docs/"
             }
           ];
     }
