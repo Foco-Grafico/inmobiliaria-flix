9a5cd1efd69d33d068b55471b95ba191defa27d4
JesusOlivas12 <chapo7504@outlook.com>
2024-05-03 16:35:47 -0600

    Cambio de pahtname

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
-import FormRegister from "./register-screen/page"
-
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <FormRegister /> 
+      pagina
     </main>
   );
 }
