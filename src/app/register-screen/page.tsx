9663d2172f85071ba616071f779f0bd874321bd3
JesusOlivas12 <chapo7504@outlook.com>
2024-05-03 16:33:29 -0600

    Implementacion de estilos al formulario de registro.

--- src/app/register-screen/page.tsx
+++ src/app/register-screen/page.tsx
@@ -1,11 +1,13 @@
 'use client'
+import React from "react";
+
 export default function FormRegister() {
     const handleSubmit = async (e:React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
 
-        const formData = new FormData (e.currentTarget);
+            const formData = new FormData (e.currentTarget);
 
-        fetch ('api/auth/register',{
+        fetch ('/api/auth/register',{
             method: 'POST',
             body: formData,
             })
@@ -23,22 +25,27 @@ export default function FormRegister() {
 
             .catch (error => {
                 console.error('Error al registrase', error.message || 'Error desconocido');
-            })
-            
-    return (
-        <form onSubmit={handleSubmit}>
-            <input type="text" name="first_name" placeholder="Juan" className="text-white"></input>
-            <input type="text" name="last_name" placeholder="Hernandez"></input>
-            <input type="email" name="email" placeholder="example@gmail,com" ></input>
-            <input type="password" name="passeword" placeholder="*******"></input>
-            <input type="text" name="phone_number" placeholder="669122133"></input>
+            })            
+        }
+return (
+    <div>
+        <div className="flex flex-col items-center justify-center p-2">
+        <h1 className="font-bold text-2xl">Registrate</h1>
+        <p className="font-semibold text-lg">Ingresa tus datos para crear una cuenta</p>    
+        </div>
+        <form onSubmit={handleSubmit} className="space-y-4 flex flex-col">
+            <label className="text-sm">Nombre</label>
+                <input className="bg-gray-100 border-gray-300 text-gray-800 placeholder:text-gray-500 rounded" type="text" name="first_name" placeholder="Juan"></input>
+            <label className="text-sm">Apellido</label>
+                <input className="bg-gray-100 border-gray-300 text-gray-800 placeholder:text-gray-500 rounded" type="text" name="last_name" placeholder="Hernandez"></input>
+            <label className="text-sm">Correo</label>
+                <input className="bg-gray-100 border-gray-300 text-gray-800 placeholder:text-gray-500 rounded" type="email" name="email" placeholder="example@gmail,com" ></input>
+            <label className="text-sm">Contraseña</label>
+                <input className="bg-gray-100 border-gray-300 text-gray-800 placeholder:text-gray-500 rounded"type="password" name="passeword" placeholder="*******"></input>
+            <label className="text-sm">Telefono</label>
+                <input className="bg-gray-100 border-gray-300 text-gray-800 placeholder:text-gray-500 rounded" type="text" name="phone_number" placeholder="+(52) 123 456 789  "></input>
             <button type="submit">Registrarse</button>
-        </form>
-    )
-
-
-
-        }
-
-        }
-
+        </form> 
+    </div>
+        )   
+}
